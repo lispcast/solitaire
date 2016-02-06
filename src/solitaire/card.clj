c6067f3db307c6c7e52ce081108ae6ae821cf127
Eric Normand <ericwnormand@gmail.com>
2016-02-06 08:44:38 -0600

    Card functional indirection

---
+++ src/solitaire/card.clj
@@ -0,0 +1,45 @@
+(ns solitaire.card)
+
+(def suits #{:diamonds :hearts :clubs :spades})
+(def values #{:A :2 :3 :4 :5 :6 :7 :8 :9 :10 :J :Q :K})
+
+(defn make [suit value]
+  (assert (contains? values value))
+  (assert (contains? suits suit))
+  [suit value])
+
+(defn suit [[suit _]]
+  suit)
+
+(defn value [[_ value]]
+  value)
+
+(defn value-numeric-value [value]
+  (case value
+    :A 1
+    :2 2
+    :3 3
+    :4 4
+    :5 5
+    :6 6
+    :7 7
+    :8 8
+    :9 9
+    :10 10
+    :J 11
+    :Q 12
+    :K 13))
+
+(defn numeric-value [card]
+  (value-numeric-value (value card)))
+
+(defn suit-color [suit]
+  (case suit
+    :diamonds :red
+    :hearts   :red
+    :clubs    :black
+    :spades   :black))
+
+(defn color [card]
+  (suit-color (suit card)))
+
--- src/solitaire/core.clj
+++ src/solitaire/core.clj
@@ -32,3 +32,46 @@
 [:clubs :3]
 
 [:card :clubs :Q]
+
+;; Card indirection
+
+(defn card [suit value]
+  [suit value])
+
+(defn card-suit [[suit _]]
+  suit)
+
+(defn card-value [[_ value]]
+  value)
+
+(defn value-numeric-value [value]
+  (case value
+    :A 1
+    :2 2
+    :3 3
+    :4 4
+    :5 5
+    :6 6
+    :7 7
+    :8 8
+    :9 9
+    :10 10
+    :J 11
+    :Q 12
+    :K 13))
+
+(defn card-numeric-value [card]
+  (value-numeric-value (card-value card)))
+
+(defn suit-color [suit]
+  (case suit
+    :diamonds :red
+    :hearts   :red
+    :clubs    :black
+    :spades   :black))
+
+(defn card-color [card]
+  (suit-color (card-suit card)))
+
+
+(card/numeric-value [:spades :K])
