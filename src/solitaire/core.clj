45a8f08b08ea7b89ca302bcc663ea6e264ac61bf
Eric Normand <ericwnormand@gmail.com>
2016-02-19 21:49:38 -0600

    Model the game.

--- src/solitaire/core.clj
+++ src/solitaire/core.clj
@@ -1,75 +1,40 @@
 (ns solitaire.core
   (:require [solitaire.card :as card]))
 
-;; Suit
+{:foundations [(list card) (list card) (list card) (list card)]
+ :stock-down (list card)
+ :stock-up (list card)
+ :tableau-up [(list card) . . .]
+ :tableau-down [(list card) . . .]}
 
-:diamonds :clubs :spades :hearts
+{:foundations [(list card) ...]
+ :stock {:down (list card)
+         :up (list card)}
+ :tableau {:down [(list card)...]
+           :up [(list card)...]}}
 
-;; Value
+{:foundations [(list card) ...]
+ :stock {:down (list card)
+         :up (list card)}
+ :tableau [{:down (list card)
+            :up (list card)} ...]}
 
-1 2 3 4 5 6 7 8 9 10 11 12 13
-:ace 2 3 4 5 6 7 8 9 10 :jack :queen :king
-:A 2 3 4 5 6 7 8 9 10 :J :Q :K
-:A :2 :3 :4 :5 :6 :7 :8 :9 :10 :J :Q :K
+;; stack
+{:down (list card) :up (list card)}
 
-;; Card
+{:foundations [(list card) ...]
+ :stock stack
+ :tableau [stack x7]}
 
-{:suit :clubs
- :value :3}
+{:foundations [(list card) ...]
+ :stock   [stack x1]
+ :tableau [stack x7]}
 
-{:suit :diamonds
- :value :6
- :type :card}
+{:foundations [stack x4]
+ :stock       [stack x1]
+ :tableau     [stack x7]}
 
-{:suit :spades
- :value :J
- :color :black}
+(first (get-in game [:foundations 0 :up]))
 
-(defrecord Card [suit value])
-
-(deftype Card [suit value])
-
-[:clubs :3]
-
-[:card :clubs :Q]
-
-;; Card indirection
-
-(defn card [suit value]
-  [suit value])
-
-(defn card-suit [[suit _]]
-   suit)
-
-(defn card-value [[_ value]]
-  value)
-
-(defn value-numeric-value [value]
-  (case value
-    :A 1
-    :2 2
-    :3 3
-    :4 4
-    :5 5
-    :6 6
-    :7 7
-    :8 8
-    :9 9
-    :10 10
-    :J 11
-    :Q 12
-    :K 13))
-
-(defn card-numeric-value [card]
-  (value-numeric-value (card-value card)))
-
-(defn suit-color [suit]
-  (case suit
-    :diamonds :red
-    :hearts   :red
-    :clubs    :black
-    :spades   :black))
-
-(defn card-color [card]
-  (suit-color (card-suit card)))
-
+;; stack
+(list {:card card :face :up} {:card card :face :down})
