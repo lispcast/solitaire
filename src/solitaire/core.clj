e00fff8e015de99ec344052a293c9563300e55af
Eric Normand <ericwnormand@gmail.com>
2016-02-28 17:27:21 -0600

    Define 2 test moves.

--- src/solitaire/core.clj
+++ src/solitaire/core.clj
@@ -1,3 +1,16 @@
 (ns solitaire.core
   (:require [solitaire.card :as card]))
 
+;; card
+;; (card/make :diamonds :2)
+
+;; stack
+;; {:down (list card) :up (list card)}
+
+;; game
+;; {:foundations [stack x4]
+;;  :stock       [stack x1]
+;;  :tableau     [stack x7]}
+
+;; move
+;; (fn [game] ... new-game)
--- test/solitaire/core_test.clj
+++ test/solitaire/core_test.clj
@@ -1,5 +1,52 @@
 (ns solitaire.core-test
   (:require [clojure.test :refer :all]
-            [solitaire.core :refer :all]))
+            [solitaire.core :refer :all]
+            [solitaire.card :as card]))
 
+(deftest tableau0-tableau1
+  (let [game {:tableau [{:up (list
+                               (card/make :diamonds :3)
+                               (card/make :clubs :4)
+                               (card/make :hearts :5))}
+                        {:up (list
+                               (card/make :spades :6))}]}
+        game2 {:tableau [{:up (list)}
+                         {:up (list
+                                (card/make :diamonds :3)
+                                (card/make :clubs :4)
+                                (card/make :hearts :5)
+                                (card/make :spades :6))}]}
+        move (fn [game]
+               (let [count 3
+                     from [:tableau 0 :up]
+                     to [:tableau 1 :up]
+                     cards (take count (get-in game from))]
+                 (-> game
+                   (update-in from (partial drop count))
+                   (update-in to into (reverse cards)))))]
+    (is (= game2 (move game)))))
 
+(deftest tableau0-foundation0
+  (let [game {:foundations [{:up (list
+                                   (card/make :diamonds :2)
+                                   (card/make :diamonds :A))}]
+              :tableau [{:up (list
+                               (card/make :diamonds :3)
+                               (card/make :clubs :4)
+                               (card/make :hearts :5))}]}
+        game2 {:foundations [{:up (list
+                                    (card/make :diamonds :3)
+                                    (card/make :diamonds :2)
+                                    (card/make :diamonds :A))}]
+               :tableau [{:up (list
+                                (card/make :clubs :4)
+                                (card/make :hearts :5))}]}
+        move (fn [game]
+               (let [count 1
+                     from [:tableau 0 :up]
+                     to [:foundations 0 :up]
+                     cards (take count (get-in game from))]
+                 (-> game
+                   (update-in from (partial drop count))
+                   (update-in to into (reverse cards)))))]
+    (is (= game2 (move game)))))
